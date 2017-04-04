0ae4ad28561b7c3966e4589f16b93b50b66c90b3
mn15104 <minhnguyen1995@googlemail.com>
2017-04-04 22:44:16 +0100

    working dynamic function

--- SemanticParser.hs
+++ SemanticParser.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE StandaloneDeriving, FlexibleInstances #-}
 import Prelude hiding (Num)
 import System.IO
 import Control.Monad
@@ -46,6 +47,10 @@ fix ff = ff (fix ff)
 
 
 
+
+
+-- new :: Loc -> Loc
+-- new n = n + 1
 ------------------------------------------
 
 data Aexp = N Num | V Var| Mult Aexp Aexp | Add Aexp Aexp | Sub Aexp Aexp
@@ -67,26 +72,24 @@ type EnvV = Var   -> Z
 
 data Config = Inter Stm State EnvP | Final State EnvP
 
+
 data Stm  = Skip | Comp Stm Stm | Ass Var Aexp | If Bexp Stm Stm | While Bexp Stm | Block DecV DecP Stm | Call Pname
       deriving (Show, Eq, Read)
-
--- new :: Loc -> Loc
--- new n = n + 1
 
 update::State->Z->Var->State
 update s i v = (\v' -> if (v == v') then i else ( s v' ) )
 
-updateV::State->(Var, Aexp)->State
-updateV s (var, aexp) = (\var' -> if (var == var') then a_val aexp s else ( s var' ) )
+updateV'::State->(Var, Aexp)->State
+updateV' s (var, aexp) = (\var' -> if (var == var') then a_val aexp s else ( s var' ) )
 
-updateV'::State->DecV->State
-updateV' s decv = foldl updateV s decv
+updateV::State->DecV->State
+updateV s decv = foldl updateV' s decv
 
-updateP::EnvP->(Pname, Stm)->EnvP
-updateP s (pname, stm) = (\pname' -> if (pname == pname') then stm else ( s pname' ) )
+updateP'::EnvP->(Pname, Stm)->EnvP
+updateP' s (pname, stm) = (\pname' -> if (pname == pname') then stm else ( s pname' ) )
 
-updateP'::EnvP->DecP->EnvP
-updateP' s decp = foldl updateP s decp
+updateP::EnvP->DecP->EnvP
+updateP s decp = foldl updateP' s decp
 
 --updateP :: (DecP, EnvV, EnvP) -> EnvP
 
@@ -113,15 +116,39 @@ ns_stm (Inter (While bexp s1) s envp)
 
 ns_stm (Inter (Block decv decp stm) s envp)  = Final s'' envp''
                                         where
-                                        s'                = updateV' s decv
-                                        envp'             = updateP' envp decp
+                                        s'                = updateV s decv
+                                        envp'             = updateP envp decp
                                         Final s'' envp''  = ns_stm(Inter stm s' envp')
 
+ns_stm (Inter (Call pname) s envp)      =   Final s' envp'
+                                        where
+                                        Final s' envp'  = ns_stm(Inter (envp pname) s envp)
 
-s_ns::Stm->State->EnvP->(State, EnvP)
-s_ns s1 s envp = (s', envp')
+-------
+s1 :: Stm
+s1 = Block [("X", N 5)] [("foo", Skip)] Skip
+
+s1' :: Stm
+s1' = Block [("x",N 0)] [("p",Ass "x" (Mult (V "x") (N 2))),("q",Call "p")] (Block [("x",N 5)] [("p",Ass "x" (Add (V "x") (N 1)))] (Call "q"))
+
+s2 :: State
+s2 _ = 0
+
+s3 :: EnvP
+s3 _ = Skip
+------
+
+s_dynamic::Stm->State->EnvP->Config
+s_dynamic stm s envp = Final stm' envp'
           where
-          Final s' envp' = ns_stm (Inter s1 s envp)
+          Final stm' envp' = ns_stm (Inter stm s envp)
+
+s_test::Config -> Integer
+s_test (Inter stm state envp) = state "x"
+s_test (Final state envp) = state "x"
+
+-- s_test (s_dynamic s1' s2 s3)
+
 
 s_ds::Stm->State->State
 s_ds ( Ass v a ) s    = update s (a_val a s) v
